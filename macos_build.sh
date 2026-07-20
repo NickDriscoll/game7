74b0962b81ae3a6c084a6c81b9c73b7455bb01b9
Nick Driscoll <nick.a.mavros@gmail.com>
2026-07-20 18:43:25 -0400

    Submodule update and commit macos vulkan loader

---
+++ macos_build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mode=$1
+build_log_level=$2
+
+if [ $# -lt 1 ]; then
+    mode="release"
+fi
+
+rm -f game7
+if [ $mode = "debug" ]; then
+    odin run ./build/build.odin -file -debug -- -l $build_log_level
+else
+    odin run ./build/build.odin -file -- -l $build_log_level
+    strip game7
+fi
